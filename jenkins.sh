2280fbd2faa1c482a09b391fed35c41598638d2d
Andy Godwin <andygodwin@sdev.co.uk>
2017-03-27 13:28:54 +0100

    Fix url for nexus snapshots.

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,7 @@ SNAPSHOT=$(echo ${VERSION} | grep -o SNAPSHOT)
 
 if [ "${SNAPSHOT}" == "SNAPSHOT" ]
 then
-    mvn -DaltDeploymentRepository=private-nexus-snapshots::default::http://pilot.eldorado.trufa.local:8081/maven-snapshots -Dmaven.test.skip=true deploy
+    mvn -DaltDeploymentRepository=private-nexus-snapshots::default::http://pilot.eldorado.trufa.local:8081/repository/maven-snapshots -Dmaven.test.skip=true deploy
 else
     mvn -DaltDeploymentRepository=private-nexus::default::http://pilot.eldorado.trufa.local:8081/repository/maven-releases -Dmaven.test.skip=true deploy
 fi
